b23cf39d5ea0099c700795a410d5909a4a87da95
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-09 20:28:46 -0500

    React setup with semantic

---
+++ client/components/RelatedEvents.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+class RelatedEvents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <Container text>
+        Related Events service
+      </Container>
+    );
+  }
+}
+
+export default RelatedEvents;
